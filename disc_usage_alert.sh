28e9c093ad6f7862f7e14d6476db9e321c7ea4ec
Dayanandachari <dayanandakv@gmail.com>
2022-03-14 14:25:18 +0000

    one more file

--- disc_usage_alert.sh
+++ disc_usage_alert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 size=`df -h . | awk -F " " 'NR>1 {print $(NF-1)}'| sed 's/%/ /g'`
 if [ $size -gt 15 ] ; then
-	echo "your disc usage has been reched treshould value $size" | mail -s "Disc usage alrert" dayanandakv@gmail.com
+	echo "your disc usage has been reched to treshould value $size" | mail -s "Disc usage alrert" dayanandakv@gmail.com
 fi
